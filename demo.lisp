2aa658912a255cf68e557a4a1f09afa0d4f86d45
root <root@red.localdomain>
2021-04-18 03:12:40 +0000

    update

--- demo.lisp
+++ demo.lisp
@@ -7,11 +7,16 @@
   ((name :initarg :name :initform (error "Name this room!") :reader name))
   (:default-initargs :client-class 'user))
 
+(defclass chat-room2 (websocket-resource)
+  ((name :initarg :name :initform (error "Name this room!") :reader name))
+  (:default-initargs :client-class 'user))
+
 (defclass user (websocket-client)
   ((name :initarg :user-agent :reader name :initform (error "Name this user!"))))
 
 (defvar *chat-rooms* (list (make-instance 'chat-room :name "/bongo")
-                           (make-instance 'chat-room :name "/fury")))
+                           (make-instance 'chat-room :name "/fury")
+                           (make-instance 'chat-room2 :name "/qwert")))
 
 (defun find-room (request)
   (find (script-name request) *chat-rooms* :test #'string= :key #'name))
@@ -22,21 +27,26 @@
   (loop for peer in (clients room)
         do (send-text-message peer (apply #'format nil message args))))
 
+(defun ps*-from-string (message)
+  (format nil "{~a}" (ps* (read-from-string message))))
+
 (defmethod client-connected ((room chat-room) user)
-  (broadcast room "~a hxsssssssssssssssssssas joined ~a" (name user) (name room))
-  (broadcast room "{console.log(\"Hello, world2\")}")
-  (broadcast room "{~a}" (ps
-			  ((@ console log) "Hello, world3")
-			  ))
   (broadcast room "~a has joined ~a" (name user) (name room)))
 
 (defmethod client-disconnected ((room chat-room) user)
   (broadcast room "~a has left ~a" (name user) (name room)))
 
-(defun ps*-from-string (message)
-  (format nil "{~a}" (ps* (read-from-string message))))
-
 (defmethod text-message-received ((room chat-room) user message)
+  (broadcast room "~a says ~a" (name user) message))
+
+(defmethod client-connected ((room chat-room2) user)
+  (broadcast room "~a hxsssssssssssssssssssas joined ~a" (name user) (name room))
+  (broadcast room "{console.log(\"Hello, world2\")}")
+  (broadcast room "{~a}" (ps
+			  ((@ console log) "Hello, world3")
+			  )))
+
+(defmethod text-message-received ((room chat-room2) user message)
   (broadcast room (if (equal (subseq message 0 1) "(")
 		      (ps*-from-string message)
 		      message)))
@@ -44,22 +54,20 @@
 (define-easy-handler (index :uri "/") () "
 <script>
 (function app(){
-    var restart = () =>	setTimeout(app, 500)
     var E = document.createElement('div')
-    function processMessage (data) {
-        E.innerHTML = data
-        var c = E.removeChild(E.firstChild)
-        document.body.appendChild(c)}
-    function evalMessage (data) {
+    var restart = () =>	setTimeout(app, 500)
+    var origin = `ws${location.origin.substr(4)}`
+    var ws = window.ws = new WebSocket(origin + '/qwert')
+    ws.onmessage = evt => {
+        var data = evt.data
         console.log(`1 >> ${data} <<`)
         if(data.startsWith('{')) eval(data)
-        else           processMessage(data)
+        else{
+            E.innerHTML = data
+            var c = E.removeChild(E.firstChild)
+            document.body.appendChild(c)}
         console.log(`9 >> ${data} <<`)}
-    var origin = `ws${location.origin.substr(4)}`
-    var ws = window.ws = new WebSocket(origin + '/fury')
-    ws.app = app
-    ws.onmessage = (evt) => evalMessage(evt.data)
-    ws.onclose   = (evt) => {console.log('x << CLOSE >>');restart()}
+    ws.onclose = () => {console.log('x << CLOSE >>');restart()}
     })()</script>")
 
 (defun main ()
